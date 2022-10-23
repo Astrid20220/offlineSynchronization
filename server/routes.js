4fcd935eaabed5c62dbfb4d3615a146c84748605
Astrid20220 <yadiragonzalez556@gmail.com>
2022-10-22 22:54:14 -0500

    API REST - Get Mensaje

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,23 @@
 var express = require('express');
 var router = express.Router();
 
+const mensajes = [
+  {
+   _id: 'XXX',
+   user: 'spiderman',
+   mensaje: 'Hola Mundo'
+
+  }
+];
+
+//Get mensajes
+router.get('/', function(req, res){
+  //res.json('Obteniendo mensajes');
+  res.json(mensajes);
+
+
+});
+
 
 
 
